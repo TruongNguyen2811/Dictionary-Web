187624fc9748ee64a6464eec2937fd8074f0cb7a
ThanhNguyen1702 <krexaid123@gmail.com>
2022-05-16 11:55:03 +0700

    dup code

--- js/edit.js
+++ js/edit.js
@@ -5,3 +5,16 @@ menuButton.addEventListener('click', ()=>{
     menu.classList.toggle("navbar-open");
     menuButton.classList.toggle("open");
 })
+
+
+
+
+const editWord =document.getElementById("newWordInput");
+
+editWord.value ="asdasd";
+console.log(editWord.value);
+
+
+const vnMean =document.getElementById("vnMeanInput");
+vnMean.textContent = "asdasd";
+console.log(vnMean.textContent);
